ca29fc2b78bf80f7f8f6c428ee1296adcf3557d1
wazeer zulfikar <wazeer.zulfikar@gmail.com>
2020-07-06 23:59:19 -0400

    multichannel images added

--- dataset.py
+++ dataset.py
@@ -38,29 +38,54 @@ def prepare_2d_tf_record_dataset(dataset_dir, tf_record_filename, glob_ext, n_im
 
     start = time.time()
 
+    start = time.time()
+    img_count = 0
+
+    start_resolution_log = int(np.log2(start_resolution))
+    target_resolution_log = int(np.log2(target_resolution))
+    resolutions = [2**res for res in range(start_resolution_log, target_resolution_log+1)]
+
+    tf_record_filenames = []
+
     for shard in range(n_shards):
         print('Creating {} / {} shard '.format(shard+1, n_shards))
         
-        img_count = 0
-        tf_record_filename = str(tf_record_save_dir.joinpath('data-%03d-of-%03d.tfrecord'%(shard+1, n_shards)))
+        tf_record_writers = {}
+        for res in resolutions:
+            tf_record_filename =  str(tf_record_save_dir.joinpath('resolution-%03d-data-%03d-of-%03d.tfrecord'%(res,
+                shard+1, n_shards)))
 
-        with tf.io.TFRecordWriter(tf_record_filename) as tf_record_writer:
+            tf_record_writers[res] = tf.io.TFRecordWriter(tf_record_filename, options=tf_record_writer_options)
 
-            for e, f in enumerate(img_filenames[img_count:img_count+n_img_per_shard]):
-                img = Image.open(f)
+        for e, f in enumerate(img_filenames[img_count:img_count+n_img_per_shard]):
+            img = Image.open(f)
 
-                img = np.array(img).astype(np.uint8)
-                img_data = img.ravel().tostring()
-                img_data = tf.image.encode_png(img).tostring()
-                img_shape = img.shape
-                if len(img_shape)==3:
-                    img_shape = np.append(img_shape, 1)
+            img = np.array(img)
 
-                tf_record_writer.write(serialize_example(img_data, img_shape))
+            img_data = (255 * (img_data - np.min(img_data)) / (np.max(img_data) - np.min(img_data)))
+            img_data_raveled = img_data.astype(np.uint8).ravel().tostring()
+            img_shape = img.shape
+            if len(img_shape)==3:
+                img_shape = np.append(img_shape, 1)
+
+            tf_record_writers[target_resolution].write(serialize_example(img_data_raveled, img_shape, img_label))
+
+            for res in range(2, target_resolution_log):
+                img_data = img_data[0::2,0::2]+img_data[0::2,1::2]+img_data[1::2,0::2]+img_data[1::2,1::2]
+                img_data = (img_data) * 0.25
+                img_data = (255 * (img_data - np.min(img_data)) / (np.max(img_data) - np.min(img_data)))
+
+                actual_resolution_log = target_resolution_log-res+1
+                actual_resolution = 2**actual_resolution_log
+
+                img_data_raveled = img_data.astype(np.uint8).ravel().tostring()
+
+                tf_record_writers[actual_resolution].write(serialize_example(img_data_raveled, img_shape, img_label))
 
                 img_count+=1
                 print('{} / {} images done'.format(img_count, n_images))
     
+        [tf_record_writers[writer].close() for writer in tf_record_writers]
         print('Time taken for shard: {}'.format(time.time()-start))
     print('Total time taken: {}'.format(time.time()-start))
 
@@ -83,6 +108,10 @@ def prepare_3d_tf_record_dataset(dataset_dir, tf_record_save_dir, glob_ext, n_im
     print('{} shards will be created'.format(n_shards))
     print('Storing {} images in each shard'.format(n_img_per_shard))
 
+    if n_images == 0:
+        print('No images found!')
+        return
+
     start = time.time()
     img_count = 0
 
@@ -102,15 +131,15 @@ def prepare_3d_tf_record_dataset(dataset_dir, tf_record_save_dir, glob_ext, n_im
 
             tf_record_writers[res] = tf.io.TFRecordWriter(tf_record_filename, options=tf_record_writer_options)
 
-        # with tf.io.TFRecordWriter(tf_record_filename, options=tf_record_writer_options) as tf_record_writer:
         for e, f in enumerate(img_filenames[img_count:img_count+n_img_per_shard]):
             img = nib.load(str(f))
             img = nib.as_closest_canonical(img)
             img_data = img.get_fdata()
             img_data = (255 * (img_data - np.min(img_data)) / (np.max(img_data) - np.min(img_data)))
             img_shape = np.array(img_data.shape).astype(np.int64)
-            # if len(img_shape) == 3:
-            #     img_data = np.expand_dims(img_data, axis=-1)
+
+            if len(img_shape) == 3:
+                img_data = np.expand_dims(img_data, axis=-1)
 
             if len(img_shape)==3:
                 img_shape = np.append(img_shape, 1)
@@ -129,8 +158,8 @@ def prepare_3d_tf_record_dataset(dataset_dir, tf_record_save_dir, glob_ext, n_im
             tf_record_writers[target_resolution].write(serialize_example(img_data_raveled, img_shape, img_label))
 
             for res in range(2, target_resolution_log):
-                img_data = img_data[0::2,0::2,0::2]+img_data[0::2,0::2,1::2]+img_data[0::2,1::2,0::2]+img_data[0::2,1::2,1::2] \
-                                +img_data[1::2,0::2,0::2]+img_data[1::2,0::2,1::2]+img_data[1::2,1::2,0::2]+img_data[1::2,1::2,1::2]
+                img_data = img_data[0::2,0::2,0::2,:]+img_data[0::2,0::2,1::2,:]+img_data[0::2,1::2,0::2,:]+img_data[0::2,1::2,1::2,:] \
+                                +img_data[1::2,0::2,0::2,:]+img_data[1::2,0::2,1::2,:]+img_data[1::2,1::2,0::2,:]+img_data[1::2,1::2,1::2,:]
                 img_data = (img_data) * 0.125
                 img_data = (255 * (img_data - np.min(img_data)) / (np.max(img_data) - np.min(img_data)))
 
